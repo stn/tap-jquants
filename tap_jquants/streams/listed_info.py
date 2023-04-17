1859e88f6a4e2867e9474f6412d85624520a98f1
Akira Ishino <akira.ishino@gmail.com>
2023-04-17 12:25:14 +0900

    Change ListedInfo from FullTableStream into IncrementalTableStream

--- tap_jquants/streams/listed_info.py
+++ tap_jquants/streams/listed_info.py
@@ -1,9 +1,8 @@
-from typing import Dict, Iterator
+from typing import Dict
 
 from singer.logger import get_logger
 
-from ..helpers import convert_json
-from .abstract import FullTableStream
+from .abstract import IncrementalTableStream
 
 LOGGER = get_logger()
 
@@ -11,7 +10,7 @@ LOGGER = get_logger()
 # https://jpx.gitbook.io/j-quants-ja/api-reference/listed_info
 
 
-class ListedInfo(FullTableStream):
+class ListedInfo(IncrementalTableStream):
     """the listed info stream"""
 
     tap_stream_id = "listed_info"
@@ -20,17 +19,15 @@ class ListedInfo(FullTableStream):
         "code",
         "date",
     ]
+    valid_replication_keys = ["date"]
+    date_window_size = 1
 
     data_key = "info"
 
-    def get_records(self) -> Iterator[Dict]:
-        """Performs API calls to extract data for each site."""
-        # 過去日付の指定について、Premiumプランでデータ提供開始日（2008年5月7日）より過去日付を指定した場合であっても、2008年5月7日時点の銘柄情報を返却します。
-        # また、指定された日付が休業日の場合は指定日の翌営業日の銘柄情報を返却します。
-        params = {
-            "date": self.config.get("date"),
+    def make_params(self, start_date: str, end_date: str, stream_metadata: Dict) -> Dict:
+        """Creates params for listed_info."""
+        code = self.config.get("code")
+        return {
+            "code": code,
+            "date": start_date,
         }
-        data = self.client.get(self.path, params)
-        # transforms data by converting camelCase fields to snake_case fields
-        transformed_records = convert_json(data)
-        yield from transformed_records.get(self.data_key, [])
