24d8b8c01a4f0be4f01e9ecf44ab01d725592ee6
Akira Ishino <akira.ishino@gmail.com>
2025-01-06 17:37:19 +0900

    revert change 0.7.1

--- tap_jquants/streams/statements.py
+++ tap_jquants/streams/statements.py
@@ -18,19 +18,3 @@ class StatementsStream(JQuantsDateStream):
     is_sorted = True
     schema_filepath = SCHEMAS_DIR / "statements.json"
     records_jsonpath = "$.statements[*]"
-
-    def post_process(
-        self,
-        row: dict,
-        _context: dict | None = None,
-    ) -> dict | None:
-        if row["changes_other_than_ones_based_on_revisions_of_accounting_standard"]:
-            row["changes_other_than_ones_based_on_revisions_of_accounting_standa"] = row["changes_other_than_ones_based_on_revisions_of_accounting_standard"]
-            del row["changes_other_than_ones_based_on_revisions_of_accounting_standard"]
-        if row["number_of_issued_and_outstanding_shares_at_the_end_of_fiscal_year_including_treasury_stock"]:
-            row["number_of_issued_and_outstanding_shares_at_the_end_of_fiscal_ye"] = row["number_of_issued_and_outstanding_shares_at_the_end_of_fiscal_year_including_treasury_stock"]
-            del row["number_of_issued_and_outstanding_shares_at_the_end_of_fiscal_year_including_treasury_stock"]
-        if row["next_year_forecast_non_consolidated_earnings_per_share2nd_quarter"]:
-            row["next_year_forecast_non_consolidated_earnings_per_share2nd_quart"] = row["next_year_forecast_non_consolidated_earnings_per_share2nd_quarter"]
-            del row["next_year_forecast_non_consolidated_earnings_per_share2nd_quarter"]
-        return row
